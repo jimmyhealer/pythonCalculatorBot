bb7085b78fdf0982f5e8f693372cc3b23fbba33e
jteng2127 <jteng2127@gmail.com>
2022-01-17 01:57:09 +0800

    add rect color

--- main.py
+++ main.py
@@ -4,9 +4,15 @@ import show
 
 speakBot = dialog.Dialog()
 
-running = True
-array = []
-cnt = 1
+# test data
+running = False
+array = [[1,1,4,4,0], [2,2,5,5,1], [3,3,6,6,2], [4,4,7,7,3], [5,5,8,8,4], [6,6,9,9,5], [7,7,10,10,6], [8,8,11,11,7], [9,9,12,12,0]]
+cnt = len(array)
+
+# running = True
+# array = []
+# cnt = 1
+
 while running:
   results = [[0, 0], [0, 0]]
   for i in range(2):
--- show.py
+++ show.py
@@ -3,17 +3,36 @@ import matplotlib.patches as patches
 import shapely.geometry as sg
 import shapely.ops as ops
 import geopandas as gpd
+import random
 import descartes
 
 class GenerateGraph:
   def __init__(self, datas):
     self.__datas = datas
+    self.__get_color = { # 紅橙黃綠藍紫黑青
+      0: (1.0, 0.0, 0.0),
+      1: (1.0, 0.5, 0.0),
+      2: (1.0, 1.0, 0.0),
+      3: (0.0, 1.0, 0.0),
+      4: (0.0, 0.0, 1.0),
+      5: (1.0, 0.0, 1.0),
+      6: (0.0, 0.0, 0.0),
+      7: (0.0, 1.0, 1.0)
+    }
+
+  def __getRandomColor():
+    random.random
 
   def showGraph(self):
-    xmin, ymin, xmax, ymax = tuple(self.__datas[0])
+    xmin, ymin, xmax, ymax = tuple(self.__datas[0][:4])
     polygons = []
+    fcs = []
     for data in self.__datas:
-      polygons.insert(0, sg.box(data[0], data[1], data[2], data[3], ))
+      polygons.insert(0, sg.box(data[0], data[1], data[2], data[3]))
+      try:
+        fcs.insert(0, self.__get_color[data[4]] + (1,))
+      except:
+        fcs.insert(0, self.__get_color[random.randint(0, 7)] + (1,))
       xmin = min(xmin, data[0], data[2])
       ymin = min(ymin, data[1], data[3])
       xmax = max(xmax, data[0], data[2])
@@ -21,10 +40,10 @@ class GenerateGraph:
 
     # boundary = gpd.GeoSeries(ops.unary_union(polygons))
     polygons.insert(0, ops.unary_union(polygons))
+    fcs.insert(0, (0,0,0,0))
     gdf = gpd.GeoDataFrame(dict(geometry=polygons))
-    fcs = [(0,1,1,1)] + [(0,0,0,0) for i in range(len(self.__datas))]
     lss = ['-'] + ['--' for i in range(len(self.__datas))]
-    lsw = [2] + [1 for i in range(len(self.__datas))]
+    lsw = [3] + [1 for i in range(len(self.__datas))]
     gdf.plot(linestyle=lss, linewidth=lsw, facecolor=fcs, edgecolor='black')
     # ax = boundary.plot(color = "aqua", edgecolor = "black")
     # for polygon in polygons:
